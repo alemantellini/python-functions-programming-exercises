103db09a15e68ae9e49186cde13b113ab24ff678
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:01:52 +0100

    Modify sum function calls for demonstration
    Updated the sum function usage to demonstrate with larger numbers.

--- exercises/02-What-is-a-function/app.py
+++ exercises/02-What-is-a-function/app.py
@@ -1,6 +1,6 @@
+#EJERCICIO 2 - What is a function
 def sum(number1,number2):
     return number1 + number2
-
 # Your code here
-total = sum(2,3)
-print(total)
+super_duper = sum(3445324,53454423)
+print(super_duper)
