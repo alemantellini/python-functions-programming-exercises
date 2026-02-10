1361c0c49b8deaf466ac0160181ab8f23b296bf0
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:06:38 +0100

    Function parameters

--- exercises/08-Function-parameters/app.py
+++ exercises/08-Function-parameters/app.py
@@ -1,7 +1,9 @@
+#EJERCICIO 8 - Function parameters
 # Your code goes here:
-def render_person(param):
-    return param
-
-
+def render_person(name, date, color, age, gender):
+    return f"{name} is a {age} years old {gender} born in {date} with {color} eyes"
 # Do not edit below this line
 print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
+# Para insertar variables en un string en Python, 
+# La forma más moderna y recomendada es usar f-strings (literales de cadena formateados), 
+# anteponiendo una f a las comillas y colocando las variables entre llaves {}.
