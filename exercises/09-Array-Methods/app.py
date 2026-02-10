da187b60a290c78e2af4c5febe11431a773dabfb
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:07:14 +0100

    Array methods

--- exercises/09-Array-Methods/app.py
+++ exercises/09-Array-Methods/app.py
@@ -1,6 +1,6 @@
+#EJERCICIO 9 - Array methods
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
-
-## CREATE YOUR FUNCTION HERE
-
-
+# CREATE YOUR FUNCTION HERE
+def sort_names(order):
+    return sorted(order)
 print(sort_names(names))
