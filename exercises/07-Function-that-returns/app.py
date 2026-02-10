d985cb273442daab5c1e2c8ddf22b99e8099ccdb
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:05:25 +0100

    Function that returns

--- exercises/07-Function-that-returns/app.py
+++ exercises/07-Function-that-returns/app.py
@@ -1,7 +1,9 @@
+#EJERCICIO 7 - Function that returns
 def dollar_to_euro(dollar_value):
 	return dollar_value * 0.91
-
 def euro_to_yen(euro_value):
 	return euro_value * 161.70
-
-####### ↓ YOUR CODE BELOW ↓ #######
+# ↓ YOUR CODE BELOW ↓ #
+euros = dollar_to_euro(137)
+yen = euro_to_yen(euros)
+print(yen)
