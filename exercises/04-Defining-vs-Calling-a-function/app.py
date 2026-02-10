6e6b82ee44d31d8068e80031afbebf95a26ce686
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:03:33 +0100

    Defining vs. Calling a function

--- exercises/04-Defining-vs-Calling-a-function/app.py
+++ exercises/04-Defining-vs-Calling-a-function/app.py
@@ -1,5 +1,7 @@
+#EJERCICIO 4 - Defining vs. Calling a function
 # Define below the function called "multi" that expects 2 parameters
-
+def multi(number1, number2):
+    return (number1 * number2)
 # Don't edit anything below this line
 return_value = multi(7,53812212)
 print(return_value)
