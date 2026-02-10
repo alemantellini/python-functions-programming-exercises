fbc8a8e26b796f5b1567ae1130170a38b370cd13
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-10 23:02:43 +0100

    Call a function

--- exercises/03-Call-a-function/app.py
+++ exercises/03-Call-a-function/app.py
@@ -1,4 +1,7 @@
+#EJERCICIO 3 - Call a function
 def calculate_area(length, width):
 	return length * width
-
 # Your code below this line
+square_area1 = calculate_area(4,4)
+square_area2 = calculate_area(2,2)
+square_area3 = calculate_area(5,5)
